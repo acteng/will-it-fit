cab81e58d54cdc19f5d75ca011a738038128c150
Dustin Carlino <dabreegster@gmail.com>
2024-06-06 17:39:32 +0100

    Stub out a WASM crate that reads GJ

---
+++ README.md
@@ -0,0 +1,11 @@
+# Will it fit?
+
+This is an experimental extension to the cross-section check tool. Given input data:
+
+- the available width on roads, at any granularity
+- a route
+- a set of cross-sections with different minimum width requirements
+
+Split the route and indicate what cross sections fit where.
+
+Attempt to do all of this purely in the browser, leveraging cloud-native file formats for reading subsets of large input data.
---
+++ backend/src/lib.rs
@@ -0,0 +1,45 @@
+use std::sync::Once;
+
+use geo::LineString;
+use geojson::de::deserialize_geometry;
+use serde::Deserialize;
+use wasm_bindgen::prelude::*;
+
+static START: Once = Once::new();
+
+#[wasm_bindgen]
+pub struct Backend {}
+
+#[wasm_bindgen]
+impl Backend {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Backend {
+        // Panics shouldn't happen, but if they do, console.log them.
+        console_error_panic_hook::set_once();
+        START.call_once(|| {
+            console_log::init_with_level(log::Level::Info).unwrap();
+        });
+
+        Backend {}
+    }
+
+    /// Takes GeoJSON with LineStrings
+    #[wasm_bindgen(js_name = exampleCall)]
+    pub fn query(&self, input: String) -> Result<String, JsValue> {
+        let input: Vec<Input> =
+            geojson::de::deserialize_feature_collection_str_to_vec(&input).map_err(err_to_js)?;
+        let linestrings: Vec<LineString> = input.into_iter().map(|x| x.geometry).collect();
+
+        Ok(format!("got {} linestrings", linestrings.len()))
+    }
+}
+
+fn err_to_js<E: std::fmt::Display>(err: E) -> JsValue {
+    JsValue::from_str(&err.to_string())
+}
+
+#[derive(Deserialize)]
+struct Input {
+    #[serde(deserialize_with = "deserialize_geometry")]
+    geometry: LineString,
+}
