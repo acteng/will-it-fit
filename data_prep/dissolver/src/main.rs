0fb89e9a722bf73a50070f19daff4eb436cd10a8
Dustin Carlino <dabreegster@gmail.com>
2024-07-27 13:20:04 +0100

    Add progress bars #8

--- data_prep/dissolver/src/main.rs
+++ data_prep/dissolver/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 use anyhow::Result;
 use geo::{BooleanOps, MultiPolygon, Polygon, Relate};
+use indicatif::{ProgressBar, ProgressStyle};
 use union_find_rs::prelude::{DisjointSets, UnionFind};
 
 // TODO Would this work faster with planar coords?
@@ -13,9 +14,13 @@ fn main() -> Result<()> {
 
     println!("Unioning {} sets", sets.len());
     let mut output = Vec::new();
+    let progress = ProgressBar::new(sets.len() as u64).with_style(ProgressStyle::with_template(
+        "[{elapsed_precise}] [{wide_bar:.cyan/blue}] {human_pos}/{human_len} ({per_sec}, {eta})").unwrap());
     for set in sets {
+        progress.inc(1);
         output.extend(union_all(&polygons, set));
     }
+    progress.finish();
     println!("Result has {}", output.len());
 
     write_polygons("out.geojson", output)?;
@@ -24,6 +29,7 @@ fn main() -> Result<()> {
 }
 
 fn read_polygons(path: &str) -> Result<Vec<Polygon>> {
+    println!("Reading {path}");
     let gj_string = std::fs::read_to_string(path)?;
     let gj: geojson::GeoJson = gj_string.parse()?;
     let mut polygons = Vec::new();
@@ -44,14 +50,18 @@ fn write_polygons(path: &str, polygons: Vec<Polygon>) -> Result<()> {
 
 // Returns disjoint sets of indices into polygons, where each set has polygons touching each other
 fn find_all_adjacencies(polygons: &Vec<Polygon>) -> Vec<HashSet<usize>> {
+    println!("Finding all adjacencies for {} polygons", polygons.len());
+    let progress = ProgressBar::new(polygons.len() as u64).with_style(ProgressStyle::with_template(
+        "[{elapsed_precise}] [{wide_bar:.cyan/blue}] {human_pos}/{human_len} ({per_sec}, {eta})").unwrap());
+
     let mut sets = DisjointSets::new();
     for i in 0..polygons.len() {
         sets.make_set(i).unwrap();
     }
 
     // TODO rstar could help
-    println!("Finding all adjacencies for {} polygons", polygons.len());
     for idx1 in 0..polygons.len() {
+        progress.inc(1);
         for idx2 in (idx1 + 1)..polygons.len() {
             let de9im = polygons[idx1].relate(&polygons[idx2]);
             if de9im.is_touches() {
@@ -59,17 +69,18 @@ fn find_all_adjacencies(polygons: &Vec<Polygon>) -> Vec<HashSet<usize>> {
             }
         }
     }
+    progress.finish();
     sets.into_iter().collect()
 }
 
 // This should usually return exactly one polygon
 fn union_all(polygons: &Vec<Polygon>, indices: HashSet<usize>) -> Vec<Polygon> {
-    println!("group with {:?}", indices);
+    //println!("group with {:?}", indices);
     let indices: Vec<usize> = indices.into_iter().collect();
     let mut result = MultiPolygon::new(vec![polygons[indices[0]].clone()]);
     for idx in indices.into_iter().skip(1) {
         result = result.union(&MultiPolygon::new(vec![polygons[idx].clone()]));
     }
-    println!("  yielded {}", result.0.len());
+    //println!("  yielded {}", result.0.len());
     result.0
 }
