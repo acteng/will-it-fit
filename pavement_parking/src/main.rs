7a6aec233f9724d62ef0bdf5e264c65a0789fbf4
Dustin Carlino <dabreegster@gmail.com>
2024-08-08 15:56:26 +0100

    Match roads to boundaries and record stats for each one

--- pavement_parking/src/main.rs
+++ pavement_parking/src/main.rs
@@ -1,12 +1,14 @@
+use std::collections::HashMap;
 use std::io::BufWriter;
 
 use anyhow::{bail, Result};
 use fs_err::File;
 use gdal::vector::LayerAccess;
 use gdal::Dataset;
-use geo::{Coord, Geometry, MapCoordsInPlace, MultiPolygon};
+use geo::{Contains, Coord, LineString, MapCoordsInPlace, MultiPolygon, Polygon};
 use geojson::{FeatureCollection, FeatureWriter};
 use indicatif::{ProgressBar, ProgressStyle};
+use rstar::{primitives::GeomWithData, RTree, RTreeObject};
 
 fn main() -> Result<()> {
     let args: Vec<String> = std::env::args().collect();
@@ -16,15 +18,21 @@ fn main() -> Result<()> {
     }
 
     let boundaries = read_boundaries("inputs/boundaries.geojson")?;
-    gpkg_to_geojson(&args[1], "out.geojson", process_feature)
+    gpkg_to_geojson(&args[1], "out.geojson", process_feature, boundaries)
 }
 
 fn gpkg_to_geojson<
-    F: Fn(Geometry, gdal::vector::Feature, &mut FeatureWriter<BufWriter<File>>) -> Result<()>,
+    F: Fn(
+        LineString,
+        gdal::vector::Feature,
+        &mut Boundaries,
+        &mut FeatureWriter<BufWriter<File>>,
+    ) -> Result<()>,
 >(
     input_path: &str,
     output_path: &str,
     process: F,
+    mut boundaries: Boundaries,
 ) -> Result<()> {
     let dataset = Dataset::open(input_path)?;
     // Assume only one layer
@@ -43,8 +51,22 @@ fn gpkg_to_geojson<
             x: trim_f64(x),
             y: trim_f64(y),
         });
+        let ls: LineString = geo.try_into()?;
 
-        process(geo, input_feature, &mut writer)?;
+        process(ls, input_feature, &mut boundaries, &mut writer)?;
+    }
+
+    // Write all boundaries with non-zero counts
+    for obj in boundaries.rtree.drain() {
+        let mut f = geojson::Feature::from(geojson::Value::from(obj.geom()));
+        let counts = boundaries.counts[&obj.data];
+        if counts[0] + counts[1] + counts[2] > 0 {
+            f.set_property("name", obj.data);
+            f.set_property("red", counts[0]);
+            f.set_property("amber", counts[1]);
+            f.set_property("green", counts[2]);
+            writer.write_feature(&f)?;
+        }
     }
 
     println!("Wrote {output_path}");
@@ -56,8 +78,9 @@ fn trim_f64(x: f64) -> f64 {
 }
 
 fn process_feature(
-    geom: Geometry,
+    geom: LineString,
     input: gdal::vector::Feature,
+    boundaries: &mut Boundaries,
     writer: &mut FeatureWriter<BufWriter<File>>,
 ) -> Result<()> {
     let Some(average) = input.field_as_double_by_name("roadwidth_average")? else {
@@ -85,11 +108,34 @@ fn process_feature(
         x => bail!("Unknown directionality {x}"),
     };
 
+    let average_rating = rating(&class, average)?;
+    let minimum_rating = rating(&class, minimum)?;
+
+    // Find all matching boundaries
+    for obj in boundaries
+        .rtree
+        .locate_in_envelope_intersecting(&geom.envelope())
+    {
+        // TODO Or even just intersects, to handle boundaries?
+        if obj.geom().contains(&geom) {
+            let count = boundaries.counts.get_mut(&obj.data).unwrap();
+            // TODO Use average_rating for now
+            if average_rating == "red" {
+                count[0] += 1;
+            } else if average_rating == "amber" {
+                count[1] += 1;
+            } else {
+                count[2] += 1;
+            }
+        }
+    }
+
+    // Include the road in the output
     let mut output_line = geojson::Feature::from(geojson::Value::from(&geom));
     output_line.set_property("average_width", average);
     output_line.set_property("minimum_width", minimum);
-    output_line.set_property("average_rating", rating(&class, average)?);
-    output_line.set_property("minimum_rating", rating(&class, minimum)?);
+    output_line.set_property("average_rating", average_rating);
+    output_line.set_property("minimum_rating", minimum_rating);
     output_line.set_property("class", class);
     output_line.set_property("direction", direction);
     writer.write_feature(&output_line)?;
@@ -126,17 +172,19 @@ fn rating(class: &str, width: f64) -> Result<&'static str> {
     }
 }
 
-struct Boundary {
-    geometry: MultiPolygon,
-    name: String,
+struct Boundaries {
+    rtree: RTree<GeomWithData<Polygon, String>>,
+    // Per boundary name, the count for [red, amber, green]
+    counts: HashMap<String, [usize; 3]>,
 }
 
-fn read_boundaries(path: &str) -> Result<Vec<Boundary>> {
+fn read_boundaries(path: &str) -> Result<Boundaries> {
     let gj: FeatureCollection = fs_err::read_to_string(path)?.parse()?;
     let mut boundaries = Vec::new();
+    let mut counts = HashMap::new();
     for f in gj.features {
         let name = f.property("name").unwrap().as_str().unwrap().to_string();
-        let geometry: MultiPolygon = if matches!(
+        let mp: MultiPolygon = if matches!(
             f.geometry.as_ref().unwrap().value,
             geojson::Value::Polygon(_)
         ) {
@@ -144,7 +192,13 @@ fn read_boundaries(path: &str) -> Result<Vec<Boundary>> {
         } else {
             f.try_into()?
         };
-        boundaries.push(Boundary { geometry, name });
+        // MultiPolygon isn't supported, so just insert multiple names
+        for polygon in mp {
+            boundaries.push(GeomWithData::new(polygon, name.clone()));
+        }
+        counts.insert(name, [0, 0, 0]);
     }
-    Ok(boundaries)
+    let rtree = RTree::bulk_load(boundaries);
+
+    Ok(Boundaries { rtree, counts })
 }
