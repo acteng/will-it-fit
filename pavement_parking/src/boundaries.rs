46d247c706627263181aee33044843af97b416b5
Dustin Carlino <dabreegster@gmail.com>
2024-08-22 09:35:11 +0100

    Sum the length of roads in each category too

--- pavement_parking/src/boundaries.rs
+++ pavement_parking/src/boundaries.rs
@@ -2,18 +2,23 @@ use std::collections::HashMap;
 use std::io::BufWriter;
 
 use anyhow::Result;
+use enum_map::EnumMap;
 use fs_err::File;
-use geo::{Contains, LineString, MultiPolygon, Polygon};
+use geo::{Contains, MultiPolygon, Polygon};
 use geojson::{Feature, FeatureCollection, FeatureWriter, Value};
 use rstar::{primitives::GeomWithData, RTree, RTreeObject};
 
-use crate::Rating;
+use crate::{Rating, Road};
 
 /// Aggregate counts per LAD and CA boundaries
 pub struct Boundaries {
     rtree: RTree<GeomWithData<Polygon, String>>,
-    // Per boundary name, the count for [red, amber, green]
-    counts: HashMap<String, [usize; 3]>,
+    info: HashMap<String, Boundary>,
+}
+
+struct Boundary {
+    counts: EnumMap<Rating, usize>,
+    total_length: EnumMap<Rating, f64>,
 }
 
 impl Boundaries {
@@ -21,7 +26,7 @@ impl Boundaries {
         println!("Reading boundaries from {path}");
         let gj: FeatureCollection = fs_err::read_to_string(path)?.parse()?;
         let mut boundaries = Vec::new();
-        let mut counts = HashMap::new();
+        let mut info = HashMap::new();
         for f in gj.features {
             let name = f.property("name").unwrap().as_str().unwrap().to_string();
             let mp: MultiPolygon =
@@ -34,26 +39,32 @@ impl Boundaries {
             for polygon in mp {
                 boundaries.push(GeomWithData::new(polygon, name.clone()));
             }
-            counts.insert(name, [0, 0, 0]);
+            info.insert(
+                name,
+                Boundary {
+                    counts: EnumMap::default(),
+                    total_length: EnumMap::default(),
+                },
+            );
         }
         println!("Building RTree of {} boundaries", boundaries.len());
         let rtree = RTree::bulk_load(boundaries);
 
         println!("");
-        Ok(Self { rtree, counts })
+        Ok(Self { rtree, info })
     }
 
-    pub fn handle_road(&mut self, geom: &LineString, rating: Rating) {
+    pub fn handle_road(&mut self, road: &Road, rating: Rating) {
         // Find all matching boundaries
-        for obj in self.rtree.locate_in_envelope_intersecting(&geom.envelope()) {
+        for obj in self
+            .rtree
+            .locate_in_envelope_intersecting(&road.geom.envelope())
+        {
             // TODO Or even just intersects, to handle boundaries?
-            if obj.geom().contains(geom) {
-                let idx = match rating {
-                    Rating::Red => 0,
-                    Rating::Amber => 1,
-                    Rating::Green => 2,
-                };
-                self.counts.get_mut(&obj.data).unwrap()[idx] += 1;
+            if obj.geom().contains(&road.geom) {
+                let info = self.info.get_mut(&obj.data).unwrap();
+                info.counts[rating] += 1;
+                info.total_length[rating] += road.length;
             }
         }
     }
@@ -62,15 +73,22 @@ impl Boundaries {
     pub fn write_output(mut self, path: &str) -> Result<()> {
         let mut summaries_writer = FeatureWriter::from_writer(BufWriter::new(File::create(path)?));
         for obj in self.rtree.drain() {
-            let mut f = Feature::from(Value::from(obj.geom()));
-            let counts = self.counts[&obj.data];
-            if counts[0] + counts[1] + counts[2] > 0 {
-                f.set_property("name", obj.data);
-                f.set_property("red", counts[0]);
-                f.set_property("amber", counts[1]);
-                f.set_property("green", counts[2]);
-                summaries_writer.write_feature(&f)?;
+            let info = &self.info[&obj.data];
+            if info.counts[Rating::Red] + info.counts[Rating::Amber] + info.counts[Rating::Green]
+                == 0
+            {
+                continue;
             }
+
+            let mut f = Feature::from(Value::from(obj.geom()));
+            f.set_property("name", obj.data);
+            f.set_property("red_count", info.counts[Rating::Red]);
+            f.set_property("amber_count", info.counts[Rating::Amber]);
+            f.set_property("green_count", info.counts[Rating::Green]);
+            f.set_property("red_length", info.total_length[Rating::Red]);
+            f.set_property("amber_length", info.total_length[Rating::Amber]);
+            f.set_property("green_length", info.total_length[Rating::Green]);
+            summaries_writer.write_feature(&f)?;
         }
         Ok(())
     }
